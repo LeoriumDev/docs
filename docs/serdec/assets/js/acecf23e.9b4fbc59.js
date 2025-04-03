68d29454baf39455d31eba66358a4dd5e748a4ab
Leorium <contact@leorium.com>
2025-04-03 22:32:29 +0800

    fix: change outputPath to serdec

--- docs/serdec/assets/js/acecf23e.9b4fbc59.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/serdec/blog","blogTitle":"Blog","authorsListPath":"/serdec/blog/authors"}')}}]);
